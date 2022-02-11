5721328018076f41b4356c2ca2ce1b1c7eefdad0
Isabella Skořepová <isabella@skorepova.info>
2022-02-11 13:08:27 +0100

    import spectacle example

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Presentation } from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Presentation />
   </React.StrictMode>,
   document.getElementById('root')
 )
