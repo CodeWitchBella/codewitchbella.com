c426f9e781e3b692fae0604d4c3a2ab110d6cf13
Isabella Skořepová <isabella@skorepova.info>
2022-02-14 19:00:13 +0100

    fix build, add dev script

--- packages/spectacle/patch.js
+++ packages/spectacle/patch.js
@@ -5,7 +5,7 @@ const fname = new URL('dist/index.js', import.meta.url)
 const initial = fs.readFileSync(fname, 'utf-8')
 
 const final = initial.replace(
-  /window\.BroadcastChannel ||/g,
+  /window\.BroadcastChannel \|\|/g,
   `(typeof window !== 'undefined' ? window.BroadcastChannel : undefined) ||`
 )
 if (final !== initial) fs.writeFileSync(fname, final)
