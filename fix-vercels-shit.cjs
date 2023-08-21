73344a9ca4c0c2e5cb703cc7ca28c7122498a2e7
Isabella Skořepová <isabella@skorepova.info>
2023-08-21 18:29:35 +0200

    style

--- fix-vercels-shit.cjs
+++ fix-vercels-shit.cjs
@@ -1,21 +1,19 @@
-/* eslint-disable no-undef */
-
 module.exports = {
   name: `fix-vercels-shit`,
   factory: (require) => {
-    const { Option } = require(`clipanion`)
-    const essentials = require("@yarnpkg/plugin-essentials").default
+    const { Option } = require(`clipanion`);
+    const essentials = require("@yarnpkg/plugin-essentials").default;
     const idx = essentials.commands.findIndex((c) =>
       c.paths?.flat().includes("add"),
-    )
-    const AddCommand = essentials.commands[idx]
+    );
+    const AddCommand = essentials.commands[idx];
     const NewAddCommand = class extends AddCommand {
-      static paths = AddCommand.paths
-      ignored = Option.Boolean(`--ignore-workspace-root-check`, false)
-    }
+      static paths = AddCommand.paths;
+      ignored = Option.Boolean(`--ignore-workspace-root-check`, false);
+    };
 
     return {
       commands: [NewAddCommand],
-    }
+    };
   },
-}
+};
