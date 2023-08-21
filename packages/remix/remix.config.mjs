3c4789b0b0173779bfb38973963c8046b21e187f
Isabella Skořepová <isabella@skorepova.info>
2023-08-21 18:00:36 +0200

    wip

---
+++ packages/remix/remix.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
+export default {
+  future: {
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
+    v2_meta: true,
+    v2_headers: true,
+    v2_routeConvention: true,
+    v2_dev: true,
+  },
+  tailwind: true,
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "cjs",
+  serverDependenciesToBundle: [
+    "@isbl/bcryptjs",
+    "@isbl/ts-utils",
+    "@isbl/react-resizer",
+  ],
+};
