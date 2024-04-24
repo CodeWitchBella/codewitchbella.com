fe7459faebe110931e7f61bb08ec0a6e95febb6b
Isabella Skořepová <isabella@skorepova.info>
2024-04-24 22:42:35 +0200

    links

--- app/routes/qr.tsx
+++ app/routes/qr.tsx
@@ -1,24 +1,34 @@
+import { useSearchParams } from "@remix-run/react";
 import { useId, useRef, useState } from "react";
 import { prose } from "~/components/css";
+import { Button } from "~/components/landing-page/button";
 import { QrPayment, iban } from "~/components/qr";
+
+function mkSpayd({ ACC, ...data }: { [key: string]: string }) {
+  console.log({ ACC, ...data });
+  if (!ACC) return "";
+  const acc = iban(ACC + "");
+  const spayd =
+    "SPD*1.0*ACC:" +
+    acc +
+    Object.entries(data)
+      .map(([k, v]) => (v ? `*${k}:${v}` : ""))
+      .join("");
+  return spayd;
+}
 
 export default function QrPage() {
   const ref = useRef<HTMLFormElement>(null);
-  const [spayd, setSpayd] = useState("");
+  const [searchParams] = useSearchParams();
+  const [spayd, setSpayd] = useState(() => {
+    return mkSpayd(Object.fromEntries(searchParams.entries()));
+  });
   const onChange = () => {
     const form = ref.current;
     if (!form) return;
-    const { ACC, ...data } = Object.fromEntries(new FormData(form).entries());
+    const data = Object.fromEntries(new FormData(form).entries());
 
-    const acc = iban(ACC + "");
-    const spayd =
-      "SPD*1.0*ACC:" +
-      acc +
-      Object.entries(data)
-        .map(([k, v]) => (v ? `*${k}:${v}` : ""))
-        .join("");
-
-    setSpayd(spayd);
+    setSpayd(mkSpayd(data as any));
   };
   return (
     <form className={prose + " mx-auto grow"} ref={ref}>
@@ -67,6 +77,7 @@ export default function QrPage() {
         defaultValue=""
       />
       <Input onChange={onChange} label="Message" name="MSG" defaultValue="" />
+      <Button type="submit">Vytvořit odkaz</Button>
       <pre>
         <code>SPAYD: {spayd.replace(/\*/g, "\n*")}</code>
       </pre>
@@ -77,6 +88,7 @@ export default function QrPage() {
 
 function Input({
   label,
+  defaultValue,
   ...props
 }: {
   name: string;
@@ -86,10 +98,20 @@ function Input({
   onChange: () => void;
 }) {
   const id = useId();
+  const [searchParams] = useSearchParams();
   return (
     <div>
       <label htmlFor={id}>{label}</label>
-      <input id={id} className="form-input dark:bg-slate-700" {...props} />
+      <input
+        id={id}
+        className="form-input dark:bg-slate-700"
+        defaultValue={
+          searchParams.has(props.name)
+            ? searchParams.get(props.name) ?? ""
+            : defaultValue
+        }
+        {...props}
+      />
     </div>
   );
 }
